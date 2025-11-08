c3f4f1df8504a561625730a37921f2adf6179515
Bohan Feng <fengb3@gmail.com>
2025-11-08 20:34:42 +0800

    Update Python CI workflow to exclude workflow files from triggering and bump package version to 0.1.1

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hachi64",
-    version="0.1.0",
+    version="0.1.1",
     author="Your Name",
     author_email="your.email@example.com",
     description="A library for custom Base64 encoding and decoding.",
