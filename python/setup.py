ddf4d7b9d216f7cd40a92ead6cab57d26d023207
Bohan Feng <fengb3@gmail.com>
2025-11-08 20:56:28 +0800

    Bump package version to 0.1.3 in setup.py

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hachi64",
-    version="0.1.2",
+    version="0.1.3",
     author="Your Name",
     author_email="your.email@example.com",
     description="A library for custom Base64 encoding and decoding.",
